7d54e573bb5bbeed253078467e7dc04c16935fd7
vominh-source <vominh272004@gmail.com>
2024-12-07 01:46:00 +0700

    Fix SignUp

--- src/app.js
+++ src/app.js
@@ -6,32 +6,17 @@ const bcrypt=require('bcrypt');
 const path = require('path');
 
 const app=express();
+
 app.use(bodyParser.json());// convert from user request to JavaScript object
 app.use(bodyParser.urlencoded({ extended: true })); // parse application/x-www-form-urlencoded
 
-// mongoose.connect('mongodb://localhost:27017/soundjack', {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-//     useCreateIndex: true
-// }).then(() => {
-//     console.log('Connected to MongoDB');
-// }).catch((error) => {
-//     console.error('Error connecting to MongoDB:', error);
-// });
+app.use('/api/auth', authRoutes);// base URL/path prefix for all routers defined in authRoutes
+
 // Phục vụ các tệp tĩnh từ thư mục "view"
 app.use(express.static(path.join(__dirname, '..', 'views'))); // Điều chỉnh đường dẫn đến thư mục 'view'
 app.get('/signup', (req, res) => {
-    res.sendFile(path.join(__dirname, '..', 'views', 'signup.html'));  // Đưa đường dẫn đúng tới signup.html
+    res.sendFile(path.join(__dirname, '..', 'views', 'signup.html'));
 });
-
-app.get('/test', (req, res) => {
-    res.type('html'); // Đảm bảo header được đặt đúng
-    res.sendFile(path.join(__dirname, '..', 'Signup', 'Signup.html'));
-});
-
-
-app.use('/api/auth', authRoutes);// base URL/path prefix for all routers defined in authRoutes
-
 
 app.listen(3000, ()=>{
     console.log('listening on port 3000');
--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -5,6 +5,7 @@ const User = require('../models/user');///////
 exports.signup=async(req, res)=> {// req: represent user request, res: represent user response 
     try{
         const {email, password}=req.body;
+        console.log("hehe")
         const user = new User({email,password});
         await user.save();//save to the moongse that have been set up in the dbUser.js and in the user.js
         res.status(201).send({message: 'User created successfully'});
