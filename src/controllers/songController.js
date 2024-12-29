7657677e71b8bf8266ef1c1b3d31506d38253332
dlnminh12 <dominh01022004@gmail.com>
2024-12-29 19:26:20 +0700

    fix song and artist homepage

--- src/controllers/songController.js
+++ src/controllers/songController.js
@@ -3,7 +3,7 @@ const Song = require('../models/song');
 exports.getSong = async (req, res) => {
     try {
         // Use MongoDB's aggregation framework to get 18 random songs
-        const resSong = await Song.aggregate([{ $sample: { size: 18 } }]);
+        const resSong = await Song.aggregate([{ $sample: { size: 12 } }]);
         //const resSong = await Song.find();
         res.json(resSong);
     } catch (error) {
